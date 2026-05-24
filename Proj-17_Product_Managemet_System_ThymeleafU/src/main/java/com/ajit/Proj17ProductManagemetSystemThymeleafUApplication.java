ce8c9c01b9e9077dd8b20dc6308c77ac20730b7e
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 23:40:19 +0530

    initial commit

---
+++ Proj-17_Product_Managemet_System_ThymeleafU/src/main/java/com/ajit/Proj17ProductManagemetSystemThymeleafUApplication.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj17ProductManagemetSystemThymeleafUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj17ProductManagemetSystemThymeleafUApplication.class, args);
+	}
+
+}
