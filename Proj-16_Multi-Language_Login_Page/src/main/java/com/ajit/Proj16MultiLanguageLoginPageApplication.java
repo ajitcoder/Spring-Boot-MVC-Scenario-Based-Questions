99a59666ce3ac54b79009d44c37b62a688f4cdcc
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 23:39:40 +0530

    initial commit

---
+++ Proj-16_Multi-Language_Login_Page/src/main/java/com/ajit/Proj16MultiLanguageLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj16MultiLanguageLoginPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj16MultiLanguageLoginPageApplication.class, args);
+	}
+
+}
---
+++ Proj-16_Multi-Language_Login_Page/src/main/java/com/ajit/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.ajit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.ajit.model.Login;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String homePage() {
+		
+		return "welcome";
+	}
+	
+	@GetMapping("/login")
+	public String loginFormPage(@ModelAttribute("signup") Login login) {
+		
+		return "loginForm";
+	}
+}
