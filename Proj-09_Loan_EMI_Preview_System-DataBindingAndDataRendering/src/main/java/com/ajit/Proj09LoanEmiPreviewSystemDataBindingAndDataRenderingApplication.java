fc6c06fe1243d179832dfe90591f3568a453b22a
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 23:34:18 +0530

    initial commit

---
+++ Proj-09_Loan_EMI_Preview_System-DataBindingAndDataRendering/src/main/java/com/ajit/Proj09LoanEmiPreviewSystemDataBindingAndDataRenderingApplication.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj09LoanEmiPreviewSystemDataBindingAndDataRenderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj09LoanEmiPreviewSystemDataBindingAndDataRenderingApplication.class, args);
+	}
+
+}
---
+++ Proj-09_Loan_EMI_Preview_System-DataBindingAndDataRendering/src/main/java/com/ajit/model/Loan.java
@@ -0,0 +1,16 @@
+package com.ajit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loan {
+
+	private Double loanAmount;
+	private Double interestRate;
+	private Double tenureMonths;
+	
+}
