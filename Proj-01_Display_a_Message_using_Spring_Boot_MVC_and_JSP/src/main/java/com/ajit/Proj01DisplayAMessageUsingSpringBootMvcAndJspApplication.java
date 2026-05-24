57791ac7d6678eb69eee5d5772226aae04f35e70
Ajit(JavaDeveloper) <ajitkr80923097@gmail.com>
2026-05-24 23:01:44 +0530

    initial commit

---
+++ Proj-01_Display_a_Message_using_Spring_Boot_MVC_and_JSP/src/main/java/com/ajit/Proj01DisplayAMessageUsingSpringBootMvcAndJspApplication.java
@@ -0,0 +1,13 @@
+package com.ajit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj01DisplayAMessageUsingSpringBootMvcAndJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj01DisplayAMessageUsingSpringBootMvcAndJspApplication.class, args);
+	}
+
+}
